b9205cd14d1756726a986aea8537e2dd073ed7d4
RobinRMC <github@robinmail.nl>
2024-05-23 21:41:38 +0200

    Add delay to prevent API spam
    This makes it less likely to get your account terminated.

--- src/plusplugins/D3SOX-userplugins/voiceChatUtilities/index.tsx
+++ src/plusplugins/D3SOX-userplugins/voiceChatUtilities/index.tsx
@@ -13,34 +13,21 @@ import type { Channel } from "discord-types/general";
 
 const VoiceStateStore = findStoreLazy("VoiceStateStore");
 
-async function runSequential<T>(promises: Promise<T>[]): Promise<T[]> {
-    const results: T[] = [];
-    for (const promise of promises) {
-        const result = await promise;
-        results.push(result);
-    }
-
-    return results;
-}
-
 function sendPatch(channel: Channel, body: Record<string, any>, bypass = false) {
     const usersVoice = VoiceStateStore.getVoiceStatesForChannel(channel.id); // Get voice states by channel id
     const myId = UserStore.getCurrentUser().id; // Get my user id
 
-    const promises: Promise<any>[] = [];
     Object.keys(usersVoice).forEach((key, index) => {
         const userVoice = usersVoice[key];
 
         if (bypass || userVoice.userId !== myId) {
-            promises.push(RestAPI.patch({
-                url: `/guilds/${channel.guild_id}/members/${userVoice.userId}`,
-                body: body
-            }));
+            setTimeout(() => {
+                RestAPI.patch({
+                    url: `/guilds/${channel.guild_id}/members/${userVoice.userId}`,
+                    body: body
+                });
+            }, index * 500);
         }
-    });
-
-    runSequential(promises).catch(error => {
-        console.error("VoiceChatUtilities failed to run", error);
     });
 }
 
