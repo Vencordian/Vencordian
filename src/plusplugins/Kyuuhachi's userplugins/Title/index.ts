dd0ea1e43fae3775db1ddbb8c55802d013e5d83e
RobinRMC <nibor.rp@gmail.com>
2024-06-19 17:55:08 +0200

    Fix

---
+++ src/plusplugins/Kyuuhachi's userplugins/Title/index.ts
@@ -0,0 +1,44 @@
+import { definePluginSettings } from "@api/Settings";
+import { Devs } from "@utils/constants";
+import definePlugin, { OptionType } from "@utils/types";
+import { findByPropsLazy } from "@webpack";
+
+const TitleManager = findByPropsLazy("setPageTitleNotificationCount", "flashPageTitle");
+const rootTitle = { base: null as string | null };
+
+export const settings = definePluginSettings({
+    title: {
+        type: OptionType.STRING,
+        default: "Vencord",
+        description: "Window title prefix",
+        onChange: setTitle,
+    },
+});
+
+function setTitle(v: string) {
+    rootTitle.base = v || null;
+    TitleManager.flashPageTitle({ messages: 0 })();
+}
+
+export default definePlugin({
+    name: "Title",
+    description: "Replaces the window title prefix",
+    authors: [Devs.Kyuuhachi],
+    settings,
+
+    patches: [
+        {
+            find: "setPageTitleNotificationCount:function()",
+            replacement: {
+                match: /(?<==)(?={base:)/,
+                replace: "$self.rootTitle??",
+            },
+        },
+    ],
+
+    start() {
+        setTitle(settings.store.title);
+    },
+
+    rootTitle,
+});
