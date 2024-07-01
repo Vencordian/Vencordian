f9f82613d54165dd8dc06e3be05c345d08e2ca8e
RobinRMC <github@robinmail.nl>
2024-07-01 10:40:20 +0200

    Update Timezones

--- src/plusplugins/Timezones/Intl.d.ts
+++ src/plusplugins/Timezones/Intl.d.ts
@@ -4,6 +4,9 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+// As vencord uses 3 decade old typescript I need to have this header file
+// see https://stackoverflow.com/questions/76400750/intl-supportedvaluesof-property-supportedvaluesof-does-not-exist-on-type-type
+// https://github.com/microsoft/TypeScript/issues/49231
 declare namespace Intl {
     type Key = "calendar" | "collation" | "currency" | "numberingSystem" | "timeZone" | "unit";
 
