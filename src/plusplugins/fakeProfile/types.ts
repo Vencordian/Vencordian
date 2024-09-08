f2e2469ff256fcdd17c33e8f62c8373771e81a37
RobinRMC <github@robinmail.nl>
2024-09-08 18:49:48 +0200

    Update fakeProfile

--- src/plusplugins/fakeProfile/types.ts
+++ src/plusplugins/fakeProfile/types.ts
@@ -31,11 +31,41 @@ export interface UserProfile extends User {
 }
 export interface UserProfileData {
     profile_effect: string;
+    custom_profile_effect?: string;
     banner: string;
     avatar: string;
     badges: Badge[];
     decoration: DecorationData;
 }
+
+export interface ProfileEffectConfig {
+    type: number;
+    id: string;
+    sku_id: string;
+    title: string;
+    description: string;
+    accessibilityLabel: string;
+    animationType: number;
+    thumbnailPreviewSrc: string;
+    reducedMotionSrc: string;
+    effects: Array<EffectConfig>;
+}
+
+export interface EffectConfig {
+    src: string;
+    loop: boolean;
+    height: number;
+    width: number;
+    duration: number;
+    start: number;
+    loopDelay: number;
+    position: {
+        x: number;
+        y: number;
+    };
+    zIndex: number;
+}
+
 
 export interface Colors {
     primary: number;
